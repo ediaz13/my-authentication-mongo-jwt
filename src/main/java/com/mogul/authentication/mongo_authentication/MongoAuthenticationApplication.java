2c765a5610ca40439db5ffa791dd8391dbce2995
ediaz13 <diazemanuel27@gmail.com>
2024-08-02 17:09:50 -0300

    ¡First Commit!

---
+++ src/main/java/com/mogul/authentication/mongo_authentication/MongoAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.mogul.authentication.mongo_authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mogul/authentication/mongo_authentication/MongoAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mogul.authentication.mongo_authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
