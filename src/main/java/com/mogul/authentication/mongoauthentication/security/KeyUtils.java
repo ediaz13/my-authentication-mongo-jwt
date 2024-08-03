92f188825d81e14d04dc0c52e7483cd63777f375
ediaz13 <diazemanuel27@gmail.com>
2024-08-03 00:43:31 -0300

    getKeyPair method

--- src/main/java/com/mogul/authentication/mongoauthentication/security/KeyUtils.java
+++ src/main/java/com/mogul/authentication/mongoauthentication/security/KeyUtils.java
@@ -1,21 +1,29 @@
 package com.mogul.authentication.mongoauthentication.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.nio.file.Files;
-import java.security.KeyFactory;
-import java.security.KeyPair;
-import java.security.PrivateKey;
-import java.security.PublicKey;
+import java.security.*;
 import java.security.interfaces.RSAPublicKey;
 import java.security.spec.EncodedKeySpec;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
+import java.util.Arrays;
 import java.util.Objects;
 
 @Component
 public class KeyUtils {
+
+    @Autowired
+    Environment environment;
+
     @Value("access-token.private")
     private String accessTokenPrivateKeyPath;
 
@@ -55,6 +63,7 @@ public class KeyUtils {
         if (publicKeyFile.exists() && privateKeyFile.exists()) {
             try {
                 KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+
                 byte[] publicKeyBytes = Files.readAllBytes(publicKeyFile.toPath());
                 EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(publicKeyBytes);
                 PublicKey publicKey = keyFactory.generatePublic(publicKeySpec);
@@ -63,18 +72,43 @@ public class KeyUtils {
                 EncodedKeySpec privateKeySpec = new X509EncodedKeySpec(privateKeyBytes);
                 PrivateKey privateKey = keyFactory.generatePrivate(privateKeySpec);
 
-
                 keyPair = new KeyPair(publicKey, privateKey);
                 return keyPair;
-            } catch (Exception e) {
+            } catch (NoSuchAlgorithmException | IOException | InvalidKeySpecException e) {
                 throw new RuntimeException("Failed to read key pair", e);
             }
 
         } else {
             // Generate the key pair
-            throw new RuntimeException("Key pair not found");
+            if (Arrays.stream(environment.getActiveProfiles()).anyMatch(env -> env.equals("prod"))) {
+                throw new RuntimeException("Key pair not found");
+            }
         }
 
+        File directory = new File("access-refresh-token-keys");
+
+        if (!directory.exists()) {
+            directory.mkdirs();
+        }
+
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            keyPair = keyPairGenerator.generateKeyPair();
+            try (FileOutputStream fos = new FileOutputStream(publicKeyPath)) {
+                X509EncodedKeySpec keySpec = new X509EncodedKeySpec(keyPair.getPublic().getEncoded());
+                fos.write(keySpec.getEncoded());
+            }
+
+            try (FileOutputStream fos = new FileOutputStream(privateKeyPath)) {
+                PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyPair.getPrivate().getEncoded());
+                fos.write(keySpec.getEncoded());
+            }
+
+            return keyPair;
+        } catch (NoSuchAlgorithmException | IOException e) {
+            throw new RuntimeException("Failed to generate key pair", e);
+        }
     }
 
     public RSAPublicKey getAccessTokenPublicKey() {
