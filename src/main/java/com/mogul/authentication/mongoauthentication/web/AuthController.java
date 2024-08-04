bcc2ba3f18f936efbd216fb69b6468d08439ae3b
ediaz13 <diazemanuel27@gmail.com>
2024-08-04 20:10:07 -0300

    Setup LoginDTO

--- src/main/java/com/mogul/authentication/mongoauthentication/web/AuthController.java
+++ src/main/java/com/mogul/authentication/mongoauthentication/web/AuthController.java
@@ -1,11 +1,13 @@
 package com.mogul.authentication.mongoauthentication.web;
 
 import com.mogul.authentication.mongoauthentication.document.User;
+import com.mogul.authentication.mongoauthentication.dto.LoginDTO;
 import com.mogul.authentication.mongoauthentication.dto.SignupDTO;
 import com.mogul.authentication.mongoauthentication.security.TokenGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,12 +27,22 @@ public class AuthController {
     @Autowired
     TokenGenerator tokenGenerator;
 
+    @Autowired
+    DaoAuthenticationProvider daoAuthenticationProvider;
+
     @PostMapping("/register")
     public ResponseEntity register(@RequestBody SignupDTO signupDTO) {
         User user = new User(signupDTO.getUsername(), signupDTO.getPassword());
         userDetailsManager.createUser(user);
 
         Authentication authentication = UsernamePasswordAuthenticationToken.authenticated(user, signupDTO.getPassword(), Collections.EMPTY_LIST);
+
+        return ResponseEntity.ok(tokenGenerator.createToken(authentication));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody LoginDTO loginDTO) {
+        Authentication authentication = daoAuthenticationProvider.authenticate(UsernamePasswordAuthenticationToken.unauthenticated(loginDTO.getUsername(), loginDTO.getPassword()));
 
         return ResponseEntity.ok(tokenGenerator.createToken(authentication));
     }
